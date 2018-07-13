d873ac90bb3e27db0ad610a328a24dc32f382d32
Badal Moradia <badal@i360llc.com>
2018-07-13 09:22:28 -0500

    Commit 2 - added data into vampire collection

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -6,11 +6,11 @@ const db = mongoose.connection;
 
 // 2. Require your model (and possibly your extra data source);
 
-const Vampire = require('./mdoels/vampire');
+const Vampire = require('./models/vampire');
 
 // 3. Connect your database and collection name
 
-mongoose.connect('mongodb://localhost:27017/vampire', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/vampire_LAB_GA', {useNewUrlParser: true});
 
 // 4. Open your mongoose connection
 
@@ -32,7 +32,52 @@ db.on('connected', () => {
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
 
+const VampireArray = require('./populateVampires');
+
+
+Vampire.create(VampireArray, (err, response) => {
+	if(err) {
+		console.log(err, ' There was an error');
+	} else {
+		console.log(response, ' Success!');
+	}
+})
+
+
 // ### Add some new vampire data
+
+const hairColorArray = ['black', 'blue', , 'red'];
+const lovesArray = [['blood', 'vampire bats'], 
+			  ['flying', 'scaring people', 'base jumping', 'red'],
+			  ['poker', 'peach schnaps'],
+			  ['history', 'King Edward II']];
+
+const nameArray = ['Francisco Orange', 'Machisimo', 'Colin Powders', 'Barnfield Broker'];
+const dobArray = ['September 14, 1745', 'March 30, 1230', 'December 9, 1946', 'February 2, 1900'];
+const eye_colorArray = ['green', 'hazel', 'peach', 'gray'];
+const locationArray = ['Maputo, Mozambique', 'New Taipei City, Taiwan', 'Minsk, Belarus', 'Ahmedabad, India'];
+const genderArray = ['m' , 'f', 'f', 'm'];
+const victimsArray = [22, 849, 49, 29291];
+
+
+for(i = 0; i < 4; i ++){
+	Vampire.create({
+		hair_color: hairColorArray[i],
+		loves: lovesArray[i],
+		name: nameArray[i],
+		dob: dobArray[i],
+		eye_color: eye_colorArray[i], 
+		location: locationArray[i],
+		gender: genderArray[i],
+		victims: victimsArray[i],
+	}, (err, response) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log(response);
+		}
+	})
+}
 
 /////////////////////////////////////////////////
 // ## QUERYING
